af7071e885e64830cde2e2e66337f72ff8e217b9
Marcel Körtgen <marcel.koertgen@gmail.com>
2020-12-27 16:49:34 +0100

    (react) js -> tsx

---
+++ react/ClientApp/src/App.tsx
@@ -0,0 +1,27 @@
+import { Component } from "react";
+import { Route } from "react-router";
+import { Layout } from "./components/Layout";
+import { Home } from "./components/Home";
+import { FetchData } from "./components/FetchData";
+import { Counter } from "./components/Counter";
+
+import "./custom.css";
+
+interface Props {}
+
+interface State {}
+
+class App extends Component<Props, State> {
+  displayName = App.name;
+  render() {
+    return (
+      <Layout>
+        <Route exact path="/" component={Home} />
+        <Route path="/counter" component={Counter} />
+        <Route path="/fetch-data" component={FetchData} />
+      </Layout>
+    );
+  }
+}
+
+export default App;
