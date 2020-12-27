aab7f82932404bd7e623fe596e891594da8818d2
Marcel Körtgen <marcel.koertgen@gmail.com>
2020-12-27 17:36:07 +0100

    (aurelia) Update

--- aurelia/ClientApp/src/main.ts
+++ aurelia/ClientApp/src/main.ts
@@ -2,7 +2,7 @@ import {Aurelia} from 'aurelia-framework';
 import * as environment from '../config/environment.json';
 import {PLATFORM} from 'aurelia-pal';
 
-export function configure(aurelia: Aurelia) {
+export function configure(aurelia: Aurelia): void {
   aurelia.use
     .standardConfiguration()
     .feature(PLATFORM.moduleName('resources/index'));
