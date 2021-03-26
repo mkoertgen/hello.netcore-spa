9251d8720ceaa11314932580e3dab676cb8335d2
Marcel Körtgen <marcel.koertgen@gmail.com>
2021-03-26 16:48:48 +0100

    (angular) Add e2e-testing

--- angular/ClientApp/e2e/protractor.conf.js
+++ angular/ClientApp/e2e/protractor.conf.js
@@ -2,31 +2,48 @@
 // Protractor configuration file, see link for more information
 // https://github.com/angular/protractor/blob/master/lib/config.ts
 
-const { SpecReporter } = require('jasmine-spec-reporter');
+const { SpecReporter } = require("jasmine-spec-reporter");
+const isDocker = require("is-docker")();
 
 /**
  * @type { import("protractor").Config }
  */
 exports.config = {
   allScriptsTimeout: 11000,
-  specs: [
-    './src/**/*.e2e-spec.ts'
-  ],
+  specs: ["./src/**/*.e2e-spec.ts"],
   capabilities: {
-    browserName: 'chrome'
+    'browserName': 'chrome',
+
+    // Run tests in a headless Chrome
+    // https://github.com/angular/protractor/blob/master/docs/browser-setup.md#using-headless-chrome
+    chromeOptions: {
+      args: isDocker ? [
+        // IMPORTANT: Required flag for running Chrome in unprivileged Docker,
+        // see https://github.com/karma-runner/karma-chrome-launcher/issues/125#issuecomment-312668593
+        '--headless', 
+        '--disable-gpu', 
+        '--no-sandbox', 
+        '--window-size=800,600'
+      ] : []
+    }
   },
+  // Fixes #5, cf.: https://github.com/angular/protractor/issues/4601#issuecomment-346943955
+  chromeDriver: isDocker ? '/usr/bin/chromedriver' : null,
+
   directConnect: true,
-  baseUrl: 'http://localhost:4200/',
-  framework: 'jasmine',
+  baseUrl: "http://localhost:4200/",
+  framework: "jasmine",
   jasmineNodeOpts: {
     showColors: true,
     defaultTimeoutInterval: 30000,
-    print: function() {}
+    print: function () {},
   },
   onPrepare() {
-    require('ts-node').register({
-      project: require('path').join(__dirname, './tsconfig.json')
+    require("ts-node").register({
+      project: require("path").join(__dirname, "./tsconfig.json"),
     });
-    jasmine.getEnv().addReporter(new SpecReporter({ spec: { displayStacktrace: true } }));
-  }
+    jasmine
+      .getEnv()
+      .addReporter(new SpecReporter({ spec: { displayStacktrace: true } }));
+  },
 };
