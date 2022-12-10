0168f9cb56de84c878eb98e77252bb3a74dfcda9
Marcel Körtgen <marcel.koertgen@gmail.com>
2022-12-10 12:17:01 +0100

    style: add cspell and conventional commit

---
+++ commitlint.config.js
@@ -0,0 +1,14 @@
+// https://commitlint.js.org/#/reference-configuration?id=javascript
+const Configuration = {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    // https://commitlint.js.org/#/reference-rules
+    "scope-enum": [
+      2,
+      "always",
+      ["angular", "aurelia", "react", "svelte", "vue"],
+    ],
+  },
+};
+
+module.exports = Configuration;
